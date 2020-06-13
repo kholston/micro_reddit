e2422c2b25a53baf8d8f98d6f19c7df473b1ac29
Ken Holston <git.kholston@gmail.com>
2020-06-13 01:58:25 -0400

    Add Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments
   belongs_to :user
   validates :title, :body, presence: true
-  validates :body, length: {minimum: 3}
+  validates :body, length: { minimum: 3 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {minimum: 3, maximum: 11}, uniqueness: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 11 }, uniqueness: true
   has_many :posts
+  has_many :comments
 end
